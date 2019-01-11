28bb2db8483f6bc5dc38baac8fe14910acc6e88b
Prasoon Sinha <prasoons.spaceo@gmail.com>
2019-01-11 18:07:09 +0530

    Test p8

--- PushNotification.go
+++ PushNotification.go
@@ -4,10 +4,13 @@ import (
 	// "all_push_pro/common"
 
 	"fmt"
+	"log"
 	"path/filepath"
 
 	"github.com/NaySoftware/go-fcm"
 	"github.com/anachronistic/apns"
+	"github.com/sideshow/apns2"
+	"github.com/sideshow/apns2/token"
 	// "github.com/sideshow/apns2"
 	// "github.com/sideshow/apns2/certificate"
 	// "github.com/sideshow/apns2/token"
@@ -66,34 +69,34 @@ func initiateIosPushWithPem(pem_path string, message string, device_token string
 	fmt.Println("Error:", resp.Error)
 }
 
-// func initiateIosPushWithP8(file_path string, message string, device_token string, keyID string, teamID string) {
-//
-// 	notification := &apns2.Notification{}
-// 	notification.DeviceToken = device_token
-// 	notification.Topic = "com.sideshow.Apns2"
-// 	notification.Payload = []byte(`{"aps":{"alert":"` + message + `"}}`)
-//
-// 	authKey, err := token.AuthKeyFromFile(file_path)
-// 	if err != nil {
-// 		log.Fatal("token error:", err)
-// 	}
-//
-// 	token := &token.Token{
-// 		AuthKey: authKey,
-// 		// KeyID from developer account (Certificates, Identifiers & Profiles -> Keys)
-// 		KeyID: keyID,
-// 		// TeamID from developer account (View Account -> Membership)
-// 		TeamID: teamID,
-// 	}
-//
-// 	client := apns2.NewTokenClient(token)
-// 	res, err := client.Push(notification)
-// 	if err != nil {
-// 		log.Fatal("Error:", err)
-// 	}
-//
-// 	fmt.Printf("%v %v %v\n", res.StatusCode, res.ApnsID, res.Reason)
-// }
+func initiateIosPushWithP8(file_path string, message string, device_token string, keyID string, teamID string) {
+
+	notification := &apns2.Notification{}
+	notification.DeviceToken = device_token
+	notification.Topic = "com.sideshow.Apns2"
+	notification.Payload = []byte(`{"aps":{"alert":"` + message + `"}}`)
+
+	authKey, err := token.AuthKeyFromFile(file_path)
+	if err != nil {
+		log.Fatal("token error:", err)
+	}
+
+	token := &token.Token{
+		AuthKey: authKey,
+		// KeyID from developer account (Certificates, Identifiers & Profiles -> Keys)
+		KeyID: keyID,
+		// TeamID from developer account (View Account -> Membership)
+		TeamID: teamID,
+	}
+
+	client := apns2.NewTokenClient(token)
+	res, err := client.Push(notification)
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+
+	fmt.Printf("%v %v %v\n", res.StatusCode, res.ApnsID, res.Reason)
+}
 
 func SendAndroidPush(device_token string, message string, fcm_key string) {
 	initiateAndroidPush(device_token, message, fcm_key)
@@ -103,9 +106,9 @@ func SendIosPushWithPem(pem_path string, message string, device_token string, ap
 	initiateIosPushWithPem(pem_path, message, device_token, apns_environment)
 }
 
-// func SendIosPushWithP8(file_path string, message string, device_token string, key_id string, team_id string) {
-// 	initiateIosPushWithP8(file_path, message, device_token, key_id, team_id)
-// }
+func SendIosPushWithP8(file_path string, message string, device_token string, key_id string, team_id string) {
+	initiateIosPushWithP8(file_path, message, device_token, key_id, team_id)
+}
 
 // func main() {
 // 	SendIosPush("home/sotsys-229/rails_projects/interstride_app/interstride/config/pushcert/production_12_dec.pem", "HI There", "40ae989435eea69c8b724de427874aaf6f375492cc5b2d46b4db5e1ebeb53400")
